bab8ea35474dbe525d3b767feb705113ea756832
Anne-Lise <annelisecoco.21@gmail.com>
2024-12-19 10:43:17 +0100

    Initial Commit

---
+++ MyAppFit/MyAppFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppFitApp.swift
+//  MyAppFit
+//
+//  Created by Apprenant 162 on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAppFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
